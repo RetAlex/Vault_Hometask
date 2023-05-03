33bbb446b2ffff87608142e19a9d9104896882fb
Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
2023-05-03 00:33:09 -0400

    Initial commit

---
+++ src/main/java/com/vault/hometask/VaultHomeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.vault.hometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultHomeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultHomeprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vault/hometask/VaultHomeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vault.hometask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultHomeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
